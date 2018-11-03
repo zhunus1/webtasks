31ce46133703ac5c375d55105ef5a9be367df77e
Zhunus <35253183+zhunus1@users.noreply.github.com>
2018-11-03 16:04:27 +0600

    Add files via upload

---
+++ week8/Task1/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="main.css">
+	<script src="script.js" defer></script>
+
+</head>
+<body>
+	<?php
+		for ($x = 0; $x <= 100; $x++) {
+  echo "Hello! <br>";
+		}
+
+	?>
+</body>
+</html>
