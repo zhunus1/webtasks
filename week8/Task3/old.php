d434b689b3d3b9eef99b1676a2a161477146c421
Zhunus <35253183+zhunus1@users.noreply.github.com>
2018-11-03 19:18:30 +0600

    Add files via upload

---
+++ week8/Task3/old.php
@@ -0,0 +1,54 @@
+<html>
+<head><style>
+.cards{display:flex;flex-wrap:wrap;width:700px;margin-left:auto;margin-right:auto;}
+.cards .card{width:250px;border:1px solid red;border-radius:5px;display:flex;margin:10px;}
+.menu {display:block;width:700px;margin-left:auto;margin-right:auto;}
+.card .title{font-size:20px;font-weight:bold;}
+.card .price{color:green;}
+.card .description{display:flex;flex-direction:column;}
+.card .price{flex-grow:1;}
+.card .year{font-weight:bold;font-size:18px;}
+.card img{width: 100px;
+			height: 100px;}
+</style></head>
+<body>
+
+<div class="cards">
+<?php
+$cars = [["maker"=>"Toyota","model"=>"Carina","year"=>2001,"price"=>20000,"image"=>"https://a.d-cd.net/cea52es-480.jpg"],["maker"=>"Toyota","model"=>"Camry","year"=>2005,"price"=>30000,"image"=>"https://d1hu588lul0tna.cloudfront.net/toyotaone/ruru/002-camry-exclusive-next-steps_tcm-3020-870114.jpg"],["maker"=>"Audi","model"=>"A8","year"=>1986,"price"=>12000,"image"=>"http://www.theautohost.com/_contentPages/vehicleContentPages/audi/2017/A8%20L/images/2017-Audi-A8L-exterior-grille.jpg"],
+["maker"=>"Audi","model"=>"A6","year"=>2005,"price"=>30000,"image"=>"https://www.cstatic-images.com/car-pictures/xl/USC60AUC021B021001.png"],
+["maker"=>"BMW","model"=>"X5","year"=>2007,"price"=>17000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],["maker"=>"BMW","model"=>"X5","year"=>2015,"price"=>19000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],["maker"=>"BMW","model"=>"Model 7","year"=>2014,"price"=>22000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],
+["maker"=>"Lada","model"=>"Granta","year"=>2017,"price"=>7000,"image"=>"http://www.kolesa.ru/uploads/2017/06/Lada-Granta-restyle-front1-1600x0-c-default.jpg"]
+];
+
+for($i = 0;$i < 8; $i++){
+	if(2018 - $cars[$i]['year'] > 7){
+	echo "<div class='cards'>";
+	echo "<div class='card'>";
+	echo "<img src=".$cars[$i]['image'] .">";
+	echo "<div class='description'>";
+	echo "<div class='title'>";
+	echo "<p>" .$cars[$i]['maker'] .$cars[$i]['model'] ."</p>";
+	echo "</div>";
+	echo "<div class='price'>";
+	echo "<p>" .$cars[$i]['price'] ."</p>";
+	echo "</div>";
+	echo "<div class='year'>";
+	echo "<p>" .$cars[$i]['year'] ."</p>";
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+} 
+}
+
+
+
+?>
+</div>
+<div class="menu">
+<a href="index3.php">Home</a>|<a href="toyota.php?maker=Toyota">Toyota</a>|<a href="bmw.php?maker=BMW">BMW</a>|<a href="audi.php?maker=Audi">Audi</a>|<a href="lada.php?maker=Lada">Lada</a></div>
+
+<div class="menu">
+<a href="old.php?year=old">Old cars (More than 7 years)</a>|<a href="expensive.php?price=expensive">Expensive cars (more than 20.000$)</a></div>
+© 2018 GitHub, Inc.
